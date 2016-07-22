srounet/Pokemon-Go-Rocket-API

commit 2a25533baa38734e011402ac4de9083f728bd2c6

Adjust console colors to my likings

PokemonGo.RocketAPI.Console/ConsoleLogger.cs
@@ -44,10 +44,10 @@ namespace PokemonGo.RocketAPI.Console
                     System.Console.ForegroundColor = ConsoleColor.Yellow;
                     break;
                 case LogLevel.Info:
-                    System.Console.ForegroundColor = ConsoleColor.Green;
+                    System.Console.ForegroundColor = ConsoleColor.Gray;
                     break;
                 case LogLevel.Debug:
-                    System.Console.ForegroundColor = ConsoleColor.Gray;
+                    System.Console.ForegroundColor = ConsoleColor.Orange;
                     break;
                 default:
                     System.Console.ForegroundColor = ConsoleColor.White;

commit 46b54a2e2e0027fc9c91df145917e04cee87d327

Fix a bug introduced in 2a25533b

PokemonGo.RocketAPI.Console/ConsoleLogger.cs
@@ -47,7 +47,7 @@ namespace PokemonGo.RocketAPI.Console
                     System.Console.ForegroundColor = ConsoleColor.Gray;
                     break;
                 case LogLevel.Debug:
-                    System.Console.ForegroundColor = ConsoleColor.Orange;
+                    System.Console.ForegroundColor = ConsoleColor.DarkYellow;
                     break;
                 default:
                     System.Console.ForegroundColor = ConsoleColor.White;