44d28e7d201762bfbe4cca3499225df1f7185fed
FeroxRev <Ferox2552@gmail.com>
2016-07-22 12:08:34 +0200

    Fix altitude setting

--- PokemonGo.RocketAPI.Console/Settings.cs
+++ PokemonGo.RocketAPI.Console/Settings.cs
@@ -16,7 +16,7 @@ namespace PokemonGo.RocketAPI.Console
         public string PtcPassword => UserSettings.Default.PtcPassword;
         public double DefaultLatitude => UserSettings.Default.DefaultLatitude;
         public double DefaultLongitude => UserSettings.Default.DefaultLongitude;
-        public double DefaultAltitude => UserSettings.Default.DefaultLongitude;
+        public double DefaultAltitude => UserSettings.Default.DefaultAltitude;
 
         ICollection<KeyValuePair<ItemId, int>> ISettings.itemRecycleFilter
         {
